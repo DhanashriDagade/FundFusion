2ada8c0cdd1d21f8452f21ff2bd46289d184b76a
Dhanashri Dagade <127673205+DhanashriDagade@users.noreply.github.com>
2025-05-12 16:47:36 +0530

    Add files via upload

---
+++ backend/routes/contact.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+
+// post contact
+router.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+  const sql = "INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)";
+  db.query(sql, [name, email, message], (err) => {
+    if (err) return res.status(500).send("Failed to save message");
+    res.send("Message received");
+  });
+});
+
+//get contact
+router.get("/contact", (req, res) => {
+  const sql = "SELECT * FROM contacts ";
+  db.query(sql, (err, results) => {
+    if (err) return res.status(500).send("Failed to retrieve contacts");
+    res.json(results);
+  });
+});
+
+module.exports = router;
